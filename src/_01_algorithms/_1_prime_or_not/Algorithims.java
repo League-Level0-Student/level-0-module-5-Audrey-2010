5a3f5cffb1f67db0c6654b1c0d377389a8aaa9d5
root <root@feaaa396a0f3>
2020-10-15 23:54:46 +0000

    algorithims

---
+++ src/_01_algorithms/_1_prime_or_not/Algorithims.java
@@ -0,0 +1,10 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Algorithims {
+public static void main(String[] args) {
+	String number=JOptionPane.showInputDialog(null, " Type in a number");
+	if (number )
+}
+}
