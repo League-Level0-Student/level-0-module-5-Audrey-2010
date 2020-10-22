f8ae56538737395963c05f67343f5f6379ad5212
root <root@6a4074548dc8>
2020-10-22 23:56:54 +0000

    goofynames

--- src/_01_algorithms/_1_prime_or_not/Algorithims.java
+++ src/_01_algorithms/_1_prime_or_not/Algorithims.java
@@ -5,6 +5,14 @@ import javax.swing.JOptionPane;
 public class Algorithims {
 public static void main(String[] args) {
 	String number=JOptionPane.showInputDialog(null, " Type in a number");
-	if (number )
+	int numberInt=Integer.parseInt(number);
+	for (int i = 2; i<numberInt; i++) {
+		if(numberInt % i ==0 ) {
+		JOptionPane.showMessageDialog(null, "Nope! This isn't a prime number");
+		System.exit(0);
+		}
+		
+	}
+	JOptionPane.showMessageDialog(null, "Yep! This is a prime number");
 }
 }
